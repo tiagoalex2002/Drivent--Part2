40b427fc4460af2e822e275619fd808f2ac541ec
Tiago <tiagoalex0202@gmail.com>
2023-07-09 10:46:06 +0100

    docs: updates

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,4 +2,5 @@ export * from './users-router';
 export * from './authentication-router';
 export * from './events-router';
 export * from './enrollments-router';
+// eslint-disable-next-line import/export
 export * from './ticket-router';
---
+++ src/services/payment-services.ts/index.ts
@@ -0,0 +1,3 @@
+import paymentRepository from '@/repositories/payments-repository';
+import { notFoundError } from '@/errors';
+import ticketRepository from '@/repositories/tickets-repository';
